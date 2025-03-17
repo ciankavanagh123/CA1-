2ffcdd83275eb732750f9026c6bdb3f2ac741af1
ciankavanagh123 <153458507+ciankavanagh123@users.noreply.github.com>
2025-03-17 14:22:08 +0000

    Add files via upload

---
+++ BloodTestApp/src/bloodtestapp/BloodTestApp.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bloodtestapp;
+
+import java.lang.ProcessHandle.Info;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author adamk
+ */
+public class BloodTestApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        BloodTestGUI myApp = new BloodTestGUI();
+        myApp.setVisible(true);
+        
+         
+     
+        
+    }
+    
+    
+    
+}
---
+++ BloodTestApp/src/bloodtestapp/Patient.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bloodtestapp;
+
+/**
+ *
+ * @author adamk
+ */
+public class Patient {
+    private String Name;
+    private String Priority;
+    private int age;
+    
+}
+
